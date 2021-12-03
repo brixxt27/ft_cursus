bdc47c0fc11d0a4583aef4b59d1c9ac37f7e5e02
brixxt27 <brixxt27@gmail.com>
2021-12-03 20:32:12 +0900

    t

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,36 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jayoon <jayoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/03 17:45:28 by jayoon            #+#    #+#             */
-/*   Updated: 2021/12/03 20:09:18 by jayoon           ###   ########.fr       */
+/*   Created: 2021/12/03 20:00:29 by jayoon            #+#    #+#             */
+/*   Updated: 2021/12/03 20:08:58 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char	*new_dst;
-	unsigned char	*new_src;
+	unsigned char	*d;
 
-	if (dst == NULL && src == NULL)
-		return (dst);
-	if (dst < src)
+	d = (unsigned char *)s;
+	while (n--)
 	{
-		new_dst = (unsigned char *)dst;
-		new_src = (unsigned char *)src;
-		ft_memcpy(dst, src, len);
+		if (*d == (unsigned char)c)
+			return (d);
+		d++;
 	}
-	else
-	{
-		new_dst = (unsigned char *)dst + len - 1;
-		new_src = (unsigned char *)src + len - 1;
-		while (len--)
-			*new_dst-- = *new_src--;
-	}
-	return (dst);
+	return (NULL);
 }
