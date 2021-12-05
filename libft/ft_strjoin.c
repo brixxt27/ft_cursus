8e8e16dd89514fbdcb6c9f0e3425c81fcee9eaf0
brixxt27 <brixxt27@gmail.com>
2021-12-05 17:54:14 +0900

    test

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,24 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jayoon <jayoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/19 15:41:34 by jayoon            #+#    #+#             */
-/*   Updated: 2021/12/05 17:35:56 by jayoon           ###   ########.fr       */
+/*   Created: 2021/12/05 17:49:39 by jayoon            #+#    #+#             */
+/*   Updated: 2021/12/05 17:54:03 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	while (*s != '\0' || *s == c)
-	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
-	}
-	return (NULL);
+	char	*str;
+	size_t	len;
+
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (s1len + s2len + 1));
+	if (str == NULL)
+		return (NULL);
+
 }
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jayoon <jayoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 23:12:48 by jayoon            #+#    #+#             */
-/*   Updated: 2021/12/05 04:16:48 by jayoon           ###   ########.fr       */
+/*   Updated: 2021/12/05 17:49:24 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*str;
 	size_t	i;
 
-	i = 0;
 	str = (char *)malloc(sizeof(char) * len + 1);
 	if (str == NULL)
 		return (NULL);
-	if (start >= ft_strlen(s))
-		return (ft_strdup(""));
-	while (i < len)
+	i = 0;
+	if (start < ft_strlen(s))
 	{
-		str[i] = s[start + i];
-		i++;
+		while (i < len)
+		{
+			*(str + i) = *(s + start + i);
+			i++;
+		}
 	}
-	str[i] = '\0';
+	*(str + i) = '\0';
 	return (str);
 }
