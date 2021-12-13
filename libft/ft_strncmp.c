e2b27bc08756f49b531e4226314ba18710d9ed23
brixxt27 <brixxt27@gmail.com>
2021-12-13 10:58:17 +0900

    Proceed my project

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jayoon <jayoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 13:41:12 by jayoon            #+#    #+#             */
-/*   Updated: 2021/12/12 16:38:43 by jayoon           ###   ########.fr       */
+/*   Updated: 2021/12/13 10:58:13 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,15 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	if (n == 0)
 		return (0);
-	while (*s1 && *s2 && n && (*s1 == *s2))
+	while (n > 0)
 	{
+		if (*s1 != *s2)
+			return (*(unsigned char *)s1 - *(unsigned char *)s2);
+		if (*s1 == '\0')
+			break ;
 		s1++;
 		s2++;
 		n--;
 	}
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	return (0);
 }
