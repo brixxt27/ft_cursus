605e10596579e1ab00a6c4e9126e30195c688f9b
brixxt27 <brixxt27@gmail.com>
2022-01-05 01:41:52 +0900

    proceed my project

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -1,29 +1,23 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jayoon <jayoon@student.42seoul.kr>         +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/30 15:18:43 by jayoon            #+#    #+#             */
-/*   Updated: 2022/01/05 00:43:07 by jayoon           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 #include <limits.h>
 
 static int	ft_isspace(int c)
 {
 	if (c == ' ' || c == '\n' || c == '\t'
-		|| c == '\v' || c == '\r' || c == '\f')
+			|| c == '\v' || c == '\r' || c == '\f')
 		return (1);
 	else
 		return (0);
 }
 
-static int	check_isspace_and_sign(const char *str, int sign)
+int ft_atoi(const char *str)
 {
+	int					digit_len;
+	int					sign;
+	unsigned long long	res;
+
+	sign = 1;
+	res = 0;
 	while (ft_isspace(*str))
 		str++;
 	if (*str == '-' || *str == '+')
@@ -32,40 +26,21 @@ static int	check_isspace_and_sign(const char *str, int sign)
 			sign = -1;
 		str++;
 	}
-	return (sign);
-}
-
-static void	cal_result(const char *str, unsigned long long *res)
-{
-	*res = *res * 10 + *str - '0';
-	str++;
-}
-
-int	ft_atoi(const char *str)
-{
-	int					sign_i[2];
-	unsigned long long	res;
-
-	sign_i[0] = 1;
-	sign_i[1] = 0;
-	res = 0;
-	sign_i[0] = check_isspace_and_sign(str, sign_i[0]);
+	digit_len = ft_strlen(str);
 	while (ft_isdigit(*str))
 	{
-		if (sign_i[1] < 19)
-			cal_result(str, &res);
-		else
-		{
-			if (sign_i[0] == -1)
-				return (0);
-			else
-				return (-1);
-		}
-		sign_i[1]++;
+		res = res * 10 + *str - '0';
+		str++;
 	}
-	if (res > LLONG_MAX + 1ULL && sign_i[0] == -1)
-		return (0);
-	else if (res > LLONG_MAX && sign_i[0] == 1)
+	if (digit_len > 19)
+	{
+		if (sign == -1)
+			return (0);
 		return (-1);
-	return ((int)res * sign_i[0]);
+	}
+	if (res > LLONG_MAX + 1ULL && sign == -1)
+		return (0);
+	else if (res > LLONG_MAX && sign == 1)
+		return (-1);
+	return ((int)res * sign);
 }
