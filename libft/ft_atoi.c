e1c351dc65ba1e5077e7dacc4fd5bf3bcc58310d
brixxt27 <brixxt27@gmail.com>
2022-01-05 20:47:27 +0900

    proceed my project

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -4,39 +4,48 @@
 static int	ft_isspace(int c)
 {
 	if (c == ' ' || c == '\n' || c == '\t'
-			|| c == '\v' || c == '\r' || c == '\f')
+		|| c == '\v' || c == '\r' || c == '\f')
 		return (1);
 	else
 		return (0);
 }
 
-int ft_atoi(const char *str)
+static unsigned long long	cal(const char *str, int *psign, int *pi)
 {
-	int					digit_len;
-	int					sign;
 	unsigned long long	res;
 
-	sign = 1;
 	res = 0;
 	while (ft_isspace(*str))
 		str++;
 	if (*str == '-' || *str == '+')
 	{
 		if (*str == '-')
-			sign = -1;
+			*psign = -1;
 		str++;
 	}
-	digit_len = ft_strlen(str);
 	while (ft_isdigit(*str))
 	{
 		res = res * 10 + *str - '0';
 		str++;
+		(*pi)++;
 	}
-	if (digit_len > 19)
+	return (res);
+}
+
+int	ft_atoi(const char *str)
+{
+	int					i;
+	int					sign;
+	unsigned long long	res;
+
+	i = 0;
+	sign = 1;
+	res = cal(str, &sign, &i);
+	if (i >= 19)
 	{
 		if (sign == -1)
-			return (0);
-		return (-1);
+			return ((int)LONG_MIN);
+		return ((int)LONG_MAX);
 	}
 	if (res > LLONG_MAX + 1ULL && sign == -1)
 		return (0);
