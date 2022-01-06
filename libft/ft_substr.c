328ee8ec12d2bb94f80278134c28f66c0c1e3de2
brixxt27 <brixxt27@gmail.com>
2022-01-06 22:23:24 +0900

    proceed my project

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,24 +6,35 @@
 /*   By: jayoon <jayoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 23:12:48 by jayoon            #+#    #+#             */
-/*   Updated: 2022/01/06 21:51:15 by jayoon           ###   ########.fr       */
+/*   Updated: 2022/01/06 22:15:47 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h" 
 
 char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char	*str;
+	size_t	i;
 	size_t	s_len;
 
-	if (!s || !len)
+	if (!s)
 		return (NULL);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
 	s_len = ft_strlen(s);
-	if (start >= s_len)
-		return (NULL);
-	else
+	if (start < s_len)
 	{
-		str = (char *)malloc(sizeof(char) * (len + 1));
+		while (i < len)
+		{
+			if (s[i] == '\0')
+				break ;
+			str[i] = s[start + i];
+			i++;
+		}
 	}
+	str[i] = '\0';
+	return (str);
 }
