d621629774d6d56f85f9bea29cc9182c34463b04
brixxt27 <brixxt27@gmail.com>
2022-01-14 16:59:29 +0900

    Proceed my project

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jayoon <jayoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 22:44:08 by jayoon            #+#    #+#             */
-/*   Updated: 2022/01/06 21:16:11 by jayoon           ###   ########.fr       */
+/*   Updated: 2022/01/14 16:03:02 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	size_t	needle_len;
 	size_t	i;
-	size_t	j;
 
-	i = ft_strlen(needle);
-	j = 0;
+	needle_len = ft_strlen(needle);
+	i = 0;
 	if (*needle == '\0')
 		return ((char *)haystack);
-	while (*(haystack + j) && i + j <= len)
+	while (haystack[i] && needle_len + i <= len)
 	{
-		if ((*(haystack + j) == *needle) && !ft_memcmp(haystack + j, needle, i))
-			return ((char *)(haystack + j));
-		j++;
+		if (haystack[i] == *(needle)
+			&& !ft_memcmp(haystack + i, needle, needle_len))
+			return ((char *)(haystack + i));
+		i++;
 	}
 	return (NULL);
 }
