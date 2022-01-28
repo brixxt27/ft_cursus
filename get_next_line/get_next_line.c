0e0f65dfdd079b86c5d8f2c26d05b9b4247f143b
brixxt27 <brixxt27@gmail.com>
2022-01-28 18:00:17 +0900

    Proceed my project

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,30 +6,40 @@
 /*   By: jayoon <jayoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 13:03:59 by jayoon            #+#    #+#             */
-/*   Updated: 2022/01/27 18:23:14 by jayoon           ###   ########.fr       */
+/*   Updated: 2022/01/28 17:23:32 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdlib.h>
+#define TRUE 1
+#define FALSE 0
 
 typedef struct s_gnl
 {
-	static char	*save;
 	char		*ret;
+	static char	*save;
 }	t_gnl;
 
-char	*read_and_save(char *ret);
-char	*cut_before_new_line(t_gnl *pset);
+char	*read_and_save(char *str);
+char	*make_two_array_based_on_new_line(t_gnl *pset);
+int		check_next_new_line(char *str);
 
 char	*get_next_line(int fd)
 {
 	t_gnl	set;
+	char	arr[BUFFER_SIZE + 1];
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	set.ret = read_and_save(set.ret);
-	if ()
-		set.ret = cut_before_new_line(&set);
+	set.ret = read_and_save(arr);
+	if (!set.ret)
+		return (NULL);
+	if (check_next_new_line(set.ret))
+	{
+		set.ret = make_two_array_based_on_new_line(&set);
+		if (!set.ret)
+			return (NULL);
+	}
 	return (set.ret);
 }
