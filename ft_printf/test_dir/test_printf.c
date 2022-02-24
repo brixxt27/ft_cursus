fa1a2c84a321f18ae7ddd56c67a4f303cb9c2b0c
brixxt27 <brixxt27@gmail.com>
2022-02-24 19:57:17 +0900

    practice using va_arg

---
+++ ft_printf/test_dir/test_printf.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("p : %p\n", (void *)16);
+	/*
+	printf("q : %q \n", 10);
+	printf("w : %w \n", 10);
+	//printf("e : %e \n", 10);
+	printf("r : %r \n", 10);
+	printf("t : %t \n", 10);
+	printf("y : %y \n", 10);
+	//printf("u : %u \n", 10);
+	//printf("i : %i \n", 10);
+	//printf("o : %o \n", 10);
+	//printf("p : %p \n", 10);
+	//printf("a : %a \n", 10);
+	//printf("s : %s \n", "hi");
+	//printf("d : %d \n", 10);
+	//printf("g : %g \n", 10);
+	printf("h : %h \n", 10);
+	printf("j : %j \n", 10);
+	printf("k : %k \n", 10);
+	printf("l : %l \n", 10);
+	printf("z : %z \n", 10);
+	//printf("x : %x \n", 10);
+	//printf("c : %c \n", 'h');
+	printf("v : %v \n", 10);
+	//printf("b : %b \n", 10);
+	//printf("n : %n \n", 10);
+	printf("m : %m \n", 10);
+	printf("; : %; \n", 10);
+	printf(", : %, \n", 10);
+	printf(". : %. \n", 10);
+	printf("/ : %/ \n", 10);
+	printf("[ : %[ \n", 10);
+	printf("] : %] \n", 10);
+	*/
+	return (0);
+}
