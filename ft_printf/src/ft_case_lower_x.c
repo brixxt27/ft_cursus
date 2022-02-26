70f6e4902ffa2afd2edb80c543e0eaa6492caaa0
brixxt27 <brixxt27@gmail.com>
2022-02-26 10:18:49 +0900

    Proceed my project

--- ft_printf/src/ft_case_lower_x.c
+++ ft_printf/src/ft_case_lower_x.c
@@ -1,24 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_case_X.c                                        :+:      :+:    :+:   */
+/*   ft_case_lower_x.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jayoon <jayoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/26 09:44:30 by jayoon            #+#    #+#             */
-/*   Updated: 2022/02/26 09:44:59 by jayoon           ###   ########.fr       */
+/*   Created: 2022/02/26 09:42:54 by jayoon            #+#    #+#             */
+/*   Updated: 2022/02/26 10:15:25 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../include/ft_printf.h"
 
-int	ft_case_X(unsigned int num)
+int	ft_case_lower_x(unsigned int num)
 {
 	int		len;
 	char	*str;
 
-	str = ft_itoa_base(num, "0123456789ABCDEF");
+	str = ft_itoa_base(num, "0123456789abcdef");
 	len = ft_case_s(str);
 	free(str);
 	return (len);
--- ft_printf/src/ft_case_upper_x.c
+++ ft_printf/src/ft_case_upper_x.c
@@ -1,23 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_case_x.c                                        :+:      :+:    :+:   */
+/*   ft_case_upper_x.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jayoon <jayoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/26 09:42:54 by jayoon            #+#    #+#             */
-/*   Updated: 2022/02/26 09:44:16 by jayoon           ###   ########.fr       */
+/*   Created: 2022/02/26 09:44:30 by jayoon            #+#    #+#             */
+/*   Updated: 2022/02/26 10:16:14 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "../include/ft_printf.h"
 
-int	ft_case_x(unsigned int num)
+int	ft_case_upper_x(unsigned int num)
 {
 	int		len;
 	char	*str;
 
-	str = ft_itoa_base(num, "0123456789abcdef");
+	str = ft_itoa_base(num, "0123456789ABCDEF");
 	len = ft_case_s(str);
 	free(str);
 	return (len);
