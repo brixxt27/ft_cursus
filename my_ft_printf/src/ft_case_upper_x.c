9410da0a5aa8eb42a3a0190fe296e8f9b1e37014
brixxt27 <brixxt27@gmail.com>
2022-03-01 16:03:35 +0900

    add Macro in ft_case_upper_x.c

--- my_ft_printf/src/ft_case_upper_x.c
+++ my_ft_printf/src/ft_case_upper_x.c
@@ -6,7 +6,7 @@
 /*   By: jayoon <jayoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 10:27:50 by jayoon            #+#    #+#             */
-/*   Updated: 2022/02/27 00:22:58 by jayoon           ###   ########.fr       */
+/*   Updated: 2022/03/01 15:45:35 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_case_upper_x(unsigned int num)
 	int		len;
 	char	*str;
 
-	str = ft_itoa_base(num, "0123456789ABCDEF");
+	str = ft_itoa_base(num, U_BASE_16);
 	len = ft_case_s(str);
 	free(str);
 	str = NULL;
