24a029734daac22d44024056aeafe03c3994d81d
brixxt27 <brixxt27@gmail.com>
2022-05-28 02:21:38 +0900

    finish to write main function of gnl

--- gnl_now/get_next_line.c
+++ gnl_now/get_next_line.c
@@ -14,19 +14,21 @@
 
 char	*get_next_line(int fd)
 {
-	static t_util	*buf;
+	static t_util	*node;
 	t_string		string;
 	char			*ret;
 	
 	if (fd < 0 || BUFFER_SIZE <=0)
 		return (NULL);
-	buf = find_node(fd);
-	init_string(&string); // 함수 안이 아니라 바깥에서 예외처리 가능, list 삭제를 바깥에서 if 로 해보자
-	if (buf->index != -1)
+	if (find_node(&node, fd) == FAIL)
+		return (NULL);
+	if (init_string(&string) == FAIL)
+		return (NULL);
+	if (node->index != -1)
 	{
-		if (copy_static_to_string(buf, &string) == EXIST)
+		if (copy_static_to_string_before_eol(node, &string) == EXIST)
 			return (copy_string_to_ret_and_add_nul(&string, ret));
 	}
-	read_and_copy_static_to_string(buf, &string);
+	read_and_copy_static_to_string(node, &string);
 	return (copy_string_to_ret_and_add_nul(&string, ret));
 }
--- gnl_now/get_next_line.h
+++ gnl_now/get_next_line.h
@@ -21,16 +21,23 @@ typedef struct s_string
 	size_t	malloc_size;
 } t_string;
 
-typedef enum e_is_there_new_line
+typedef enum e_is_there_eol
 {
 	EXIST = 0,
 	NOT_EXIXT
 } t_nl;
 
+typedef enum e_status
+{
+	SUCCESS = 0,
+	FAIL
+} t_stat;
+
 char	*get_next_line(int fd);
-t_util	*find_node(int fd);
-t_nl	copy_static_to_string(t_util *buf, t_string *ps);
+t_util	*find_node(t_util **pn, int fd);
+t_stat	init_string(t_string *ps);
+t_nl	copy_static_to_string_before_eol(t_util *node, t_string *ps);
 char	*copy_string_to_ret_and_add_nul(t_string *ps, char *ret);
-void	read_and_copy_static_to_string(t_util *buf, t_string *ps);
+void	read_and_copy_static_to_string(t_util *node, t_string *ps);
 
 #endif
