1a26dc0a5375b06211a4297ea604b5efd28f0158
brixxt27 <brixxt27@gmail.com>
2022-05-28 01:58:24 +0900

    write main function

--- gnl_now/get_next_line.c
+++ gnl_now/get_next_line.c
@@ -12,10 +12,21 @@
 
 #include "get_next_line.h"
 
-t_util	*find_node(int fd);
-
 char	*get_next_line(int fd)
 {
+	static t_util	*buf;
+	t_string		string;
+	char			*ret;
+	
 	if (fd < 0 || BUFFER_SIZE <=0)
 		return (NULL);
+	buf = find_node(fd);
+	init_string(&string); // 함수 안이 아니라 바깥에서 예외처리 가능, list 삭제를 바깥에서 if 로 해보자
+	if (buf->index != -1)
+	{
+		if (copy_static_to_string(buf, &string) == EXIST)
+			return (copy_string_to_ret_and_add_nul(&string, ret));
+	}
+	read_and_copy_static_to_string(buf, &string);
+	return (copy_string_to_ret_and_add_nul(&string, ret));
 }
--- gnl_now/get_next_line.h
+++ gnl_now/get_next_line.h
@@ -11,8 +11,7 @@ typedef struct s_util
 {
 	int		fd;
 	char	buf[BUFFER_SIZE];
-	char	*cur;
-	char	*end;
+	int		index;
 } t_util;
 
 typedef struct s_string
@@ -21,5 +20,17 @@ typedef struct s_string
 	size_t	len;
 	size_t	malloc_size;
 } t_string;
+
+typedef enum e_is_there_new_line
+{
+	EXIST = 0,
+	NOT_EXIXT
+} t_nl;
+
+char	*get_next_line(int fd);
+t_buf	*find_node(int fd);
+t_nl	copy_static_to_string(t_util *buf, t_string *ps);
+char	*copy_string_to_ret_and_add_nul(t_string *ps, char *ret);
+void	read_and_copy_static_to_string(t_util *buf, t_string *ps);
 
 #endif
