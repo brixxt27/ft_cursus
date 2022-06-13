320827d3cb0e8ada3a938a357f1906ca34c93988
brixxt27 <brixxt27@gmail.com>
2022-06-13 10:52:23 +0900

    Add 42 header. Write parse_argv in pipex.c

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jayoon <jayoon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/13 10:26:56 by jayoon            #+#    #+#             */
+/*   Updated: 2022/06/13 10:46:10 by jayoon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 	/*
 	//순서
 	명령행 인자로 프로세스와 옵션 받기
@@ -15,7 +27,7 @@
 #include <stdio.h>
 
 int		print_err(const char *str);
-void	parse_argv(t_list *p_list, char **argv);
+void	parse_argv(t_list *p_list, char **argv, char **envp);
 // void	proc_child(void);
 
 int	main(int argc, char *argv[], char *envp[])
@@ -24,8 +36,8 @@ int	main(int argc, char *argv[], char *envp[])
 	t_list	list;
 
 	if (argc < 2) // 프로그램 이름, 명령어와 옵션 포함한 문자열
-		return (print_err("argc"));
-	parse_argv(&list, argv);
+		print_err("argc");
+	parse_argv(&list, argv, envp);
 	// pid = fork();
 	// if (pid == 0)
 	// 	proc_child();
@@ -37,14 +49,22 @@ int	main(int argc, char *argv[], char *envp[])
 int		print_err(const char *str)
 {
 	perror(str);
-	return (1);
+	exit(1);
 }
 
-void	parse_argv(t_list *p_list, char **argv)
+void	parse_argv(t_list *p_list, char **argv, char **envp)
 {
-	// argv[1] 스페이스 바 기준으로 split, NULL 로 끝나는 2차원 배열에 하나씩 넣기
+	char	*path;
+
+	path = NULL;
 	p_list->execve_argv = ft_split(argv[1], ' ');
-	// path envp 에서 PATH: 로 시작하는 문자열에서 : 기준으로 split
+	while (*envp)
+	{
+		if (!ft_strncmp(*envp, "PATH=", 5))
+			path = *envp;
+	}
+	if (path == NULL)
+
 	// execve_argv[0] 앞에 붙여 넣으며 존재하는 명령어인지 확인
 	// if 있으면 실행
 	// else 에러 메시지 띄우며 종료 
---
+++ pipex/test_perror.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    perror("str");
+    return (0);
+}
