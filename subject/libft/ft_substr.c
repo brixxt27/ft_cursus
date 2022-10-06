8cd9ca5433549aa2887d239c5991e958ac464f00
“brixxt27” <brixxt27@gmail.com>
2022-10-06 20:18:03 +0900

    fix: SIGSEGV substr

--- subject/libft/ft_substr.c
+++ subject/libft/ft_substr.c
@@ -6,34 +6,28 @@
 /*   By: jayoon <jayoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 23:12:48 by jayoon            #+#    #+#             */
-/*   Updated: 2022/01/13 14:02:10 by jayoon           ###   ########.fr       */
+/*   Updated: 2022/10/06 20:08:57 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h" 
 
-char	*ft_substr(const char *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*str;
-	size_t	i;
-	size_t	s_len;
+	char			*str;
+	size_t			i;
+	unsigned int	s_length;
 
-	if (!s)
-		return (NULL);
+	s_length = ft_strlen(s);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == 0)
+		return (0);
 	i = 0;
-	s_len = ft_strlen(s);
-	if (s_len < len)
-		str = (char *)malloc(sizeof(char) * (s_len + 1));
-	else
-		str = (char *)malloc(sizeof(char) * (len + 1));
-	if (!str)
-		return (NULL);
-	while (i < len && start < s_len)
+	while (start < s_length && s[start] != '\0' && len > i)
 	{
-		if (s[i] == '\0')
-			break ;
-		str[i] = s[start + i];
-		i++;
+		str[i] = s[start];
+		++i;
+		++start;
 	}
 	str[i] = '\0';
 	return (str);
