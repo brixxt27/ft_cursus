b1542e6d46bf47bea64b2145605d21232ae389ed
brixxt27 <brixxt27@gmail.com>
2023-01-31 22:07:48 +0900

    feat: exam 보고 나와서 commit,

--- my_exam/rank_04_microshell/main.c
+++ my_exam/rank_04_microshell/main.c
@@ -1,6 +1,8 @@
 #include "microshell.h"
 #include <unistd.h>
 #include <string.h>
+//err
+#include <stdio.h>
 
 int	ft_strlen(char* str)
 {
@@ -21,10 +23,8 @@ int	ft_putstr_err(char* str)
 	return 1;
 }
 
-int	init_execve_info(char** argv, t_execve_info* execve_info, int last_argv_idx)
+int	init_execve_info(char** argv, t_execve_info* execve_info, int i)
 {
-	int i = last_argv_idx + 1;
-
 	execve_info->path = argv[i];
 	execve_info->argv = &argv[i];
 	while (1) {
@@ -36,11 +36,21 @@ int	init_execve_info(char** argv, t_execve_info* execve_info, int last_argv_idx)
 			else
 				execve_info->type = e_semicolon;
 			argv[i + 1] = NULL;
+			i++;
 			break;
 		}
 		i++;
 	}
 	return i;
+}
+
+void	do_it_child(t_execve_info* execve_info, t_pipe* pipe_info)
+{
+	if (pipe_info->pipe_fd[0] != 0)
+		close(pipe_info->pipe_fd[0]);
+	dup2(pipe_info->pipe_fd[1], 1);
+	execve(execve_info->path, execve_info->argv, execve_info->envp);
+	ft_putstr_err("error: fatal\n");
 }
 
 int	main(int argc, char* argv[], char* envp[])
@@ -51,22 +61,36 @@ int	main(int argc, char* argv[], char* envp[])
 									envp,
 									e_null
 	};
-	int		i = 0;
-	int		last_argv_idx = 0;
+	int		i = 1;
+	int		ret;
 	pid_t	pid;
+	t_pipe	pipe_info = {{0, 1}, 0};
 
 	if (argc < 2)
 		return 0;
 	
-	while (argv[i] != NULL) {
-		last_argv_idx = init_execve_info(argv, &execve_info, last_argv_idx);
+	while (argc > i) {
+		i = init_execve_info(argv, &execve_info, i);
+		
+		// test
+		//printf("type: %d\n", execve_info.type);
+		if (execve_info.type == e_pipe) {
+			ret = pipe(pipe_info.pipe_fd);
+			if (ret == -1) {
+				ft_putstr_err("error: fatal\n");
+				return 1;
+			}
+		}
+
 		pid = fork();
 		if (pid == -1)
 			return ft_putstr_err("error: fatal\n");
 		if (pid == 0) {
-			//do_it_child();
+			do_it_child(&execve_info, &pipe_info);
+			return 1;
 		}
 		//do_it_parent();
+
 		i++;
 	}
 }
--- my_exam/rank_04_microshell/microshell.h
+++ my_exam/rank_04_microshell/microshell.h
@@ -8,18 +8,26 @@
 #define	STDOUT	1
 #define	STDERR	2
 
-enum e_type {
+enum e_type
+{
 	e_pipe,
 	e_semicolon,
 	e_null
 };
 
-typedef struct s_execve_info {
+typedef struct s_execve_info
+{
 	char*		path;
 	char**		argv;
 	char**		envp;
 	enum e_type	type;
 }	t_execve_info;
+
+typedef struct s_pipe_fd
+{
+	int	pipe_fd[2];
+	int	past_fd;
+}	t_pipe;
 
 //enum e_isword {
 //	e_noword,
